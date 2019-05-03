bd6ecf4f2f7e36c8c13bed9bfe6f241b84ddf71a
taylorfiorelli <taylor.fiorelli@greenwoodcollege.com>
2019-05-02 22:58:34 -0400

    log in added need to add login blocker

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 # https://pythonhow.com/how-a-flask-app-works/
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, redirect, url_for, request
 from camara import VideoCamera
 
 app = Flask(__name__)
 
 
 @app.route('/')
+def gotologin():
+    return redirect(url_for('login'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    return redirect(url_for('login'))
+
+
+@app.route('/home')
 def home():
     return render_template('home.html')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 def gen(camera):
@@ -17,13 +44,7 @@ def gen(camera):
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
-@app.route('/video_feed/')
-def video_feed():
-    return Response(gen(VideoCamera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-@app.route('/about/')
+@app.route('/about')
 def about():
     return render_template('about.html')
 
