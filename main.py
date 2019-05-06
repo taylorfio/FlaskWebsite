10054f062ae1faaae297f22af81062847ed456ce
taylorfiorelli <taylor.fiorelli@greenwoodcollege.com>
2019-05-06 12:05:53 -0400

    atenpt at login_required

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # https://pythonhow.com/how-a-flask-app-works/
-from flask import Flask, render_template, Response, redirect, url_for, request
+from flask import Flask, render_template, Response, redirect, url_for, request, flash
 from camara import VideoCamera
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
 
 app = Flask(__name__)
-colour = 'light'
 
 
 def gen(camera):
@@ -11,6 +11,41 @@ def gen(camera):
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+# flask-login
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class User(UserMixin):
+    # proxy for a database of users
+    user_database = {"JohnDoe": ("JohnDoe", "John"),
+               "JaneDoe": ("JaneDoe", "Jane")}
+
+    def __init__(self, username, password):
+        self.id = username
+        self.password = password
+
+    @classmethod
+    def get(cls,id):
+        return cls.user_database.get(id)
+
+
+@login_manager.request_loader
+def load_user(request):
+    token = request.headers.get('Authorization')
+    if token is None:
+        token = request.args.get('token')
+
+    if token is not None:
+        username, password = token.split(":")  # naive token
+        user_entry = User.get(username)
+        if user_entry is not None:
+            user = User(user_entry[0], user_entry[1])
+            if user.password == password:
+                return user
+    return None
 
 
 @app.route('/')
@@ -29,39 +64,34 @@ def login():
     return render_template('login.html', error=error)
 
 
+
 @app.route('/logout')
+@login_required
 def logout():
     return redirect(url_for('login'))
 
 
 @app.route('/home')
+@login_required
 def home():
     return render_template('home.html')
 
 
 @app.route('/video_feed')
+@login_required
 def video_feed():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/about')
+@login_required
 def about():
     return render_template('about.html')
 
 
-@app.route('/colourmode')
-def colourmode():
-    global colour
-    if colour == 'light':
-        colour = 'dark'
-        return render_template('home.html', color=colour)
-    elif colour == 'dark':
-        colour = 'light'
-        return render_template('home.html', color=colour)
-
-
 if __name__ == '__main__':
+    app.config["SECRET_KEY"] = 'ITSASECRET'
     app.run(host='0.0.0.0', debug=True)
 
 # add login blocker
