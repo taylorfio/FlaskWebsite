781d6ca33258fb24175db7918c674e7d4119f068
taylorfiorelli <taylor.fiorelli@greenwoodcollege.com>
2019-05-16 14:27:11 -0400

    last

--- bettertest.py
+++ bettertest.py
@@ -2,7 +2,7 @@ import RPi.GPIO as gpio
 import time
 
 
-def init():
+def init():  # creates a callable gpio set up for motor with wheel
     gpio.setmode(gpio.BCM)
     gpio.setup(17, gpio.OUT)
     gpio.setup(22, gpio.OUT)
@@ -10,44 +10,43 @@ def init():
     gpio.setup(24, gpio.OUT)
 
 
-def forward(sec):  # Turn motor forwards
-    init()
-    gpio.output(17, True)
+def forward(sec):  # orders gpio pins to create forwards motion
+    init()  # calls gpio set up
+    gpio.output(17, True)  # turns on True gpio
     gpio.output(22, False)
     gpio.output(23, True)
     gpio.output(24, False)
     time.sleep(sec)
-    gpio.cleanup()
+    gpio.cleanup()  # resets the gpio pins
 
 
-def reverse(sec):  # Turn motor reverse
-    init()
-    gpio.output(17, False)
+def reverse(sec):  # orders gpio pins to create backwards motion
+    init()  # calls gpio set up
+    gpio.output(17, False)  # turns on True gpio
     gpio.output(22, True)
     gpio.output(23, False)
     gpio.output(24, True)
     time.sleep(sec)
-    gpio.cleanup()
+    gpio.cleanup()  # resets the gpio pins
 
 
 def motor_on():
     gpio.setmode(gpio.BCM)
-    gpio.setup(21, gpio.OUT)
+    gpio.setup(21, gpio.OUT)  # calls gpio pin
     gpio.output(21, gpio.HIGH)  # Turn relay motor on
-    time.sleep(1)
+    time.sleep(1)  # waits so it runs smoothly
     gpio.output(21, gpio.LOW)  # Turn relay motor off
-    time.sleep(3)
-    gpio.cleanup()
+    time.sleep(3)  # waits three second while it runs
+    gpio.cleanup()  # resets the gpio pins to turn it off
 
 
-x = True
-while x:
-    textinput = input(str("left, right or fire"))
+while True:  # runs forever
+    textinput = input(str("left, right or fire"))  # takes your input
     if textinput == 'left':
-        reverse(1)
+        reverse(1)  # runs motor reverse for 1 second
     elif textinput == 'right':
-        forward(1)
+        forward(1)  # runs motor forward for 1 second
     elif textinput == 'fire':
-        motor_on()
+        motor_on()  # runs the motor controlled by a relay
     else:
-        print("error")
+        print("error")  # defencive coding
